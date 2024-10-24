22054b52eac8cbe9e38f7f47f9e64f6bae228329
Kaamkiya <ziam3.1415@gmail.com>
2024-10-24 17:23:14 -0400

    style: Use tabs for indentation
    According to the suckless style guide, as well as the official
    kernel.org guidelines, both linked below, tabs are better than spaces.
    I agree and wish I knew before that there are better style guides than
    Google's.
    
    - Suckless: https://suckless.org/coding_style/
    - Kernel: https://www.kernel.org/doc/Documentation/process/coding-style.rst

--- 2015/day1.c
+++ 2015/day1.c
@@ -3,31 +3,31 @@
 
 /* Not Quite Lisp: https://adventofcode.com/2015/day/1 */
 int main() {
-  char input[16384];
-  FILE *inputfile = fopen("day1.txt", "r");
-  if (inputfile == NULL) {
-    perror("Failed to open input file.");
-    return 1;
-  }
+	char input[16384];
+	FILE *inputfile = fopen("day1.txt", "r");
+	if (inputfile == NULL) {
+		perror("Failed to open input file.");
+		return 1;
+	}
 
-  fscanf(inputfile, "%s", &input);
-  fclose(inputfile);
+	fscanf(inputfile, "%s", &input);
+	fclose(inputfile);
 
-  int floor = 0;
-  int basement_entry = -1;
+	int floor = 0;
+	int basement_entry = -1;
 
-  for (int i = 0; i < strlen(input); i++) {
-    if (input[i] == '(') {
-      floor++;
-    } else {
-      floor--;
-    }
+	for (int i = 0; i < strlen(input); i++) {
+		if (input[i] == '(') {
+			floor++;
+		} else {
+			floor--;
+		}
 
-    if (basement_entry < 0 && floor < 0) {
-      basement_entry = i + 1;
-    }
-  }
-  printf("Floor: %d\n", floor);
-  printf("Entered basement: %d\n", basement_entry);
-  return 0;
+		if (basement_entry < 0 && floor < 0) {
+			basement_entry = i + 1;
+		}
+	}
+	printf("Floor: %d\n", floor);
+	printf("Entered basement: %d\n", basement_entry);
+	return 0;
 }
--- 2015/day2.c
+++ 2015/day2.c
@@ -1,61 +1,62 @@
 #include <stdio.h>
 #include <string.h>
 
-unsigned int smallest_area (unsigned int area0, unsigned int area1, unsigned int area2) {
-  if (area0 <= area1 && area0 <= area2) {
-    return area0;
-  }
-  if (area1 <= area0 && area1 <= area2) {
-    return area1;
-  }
-  return area2;
+unsigned int smallest_area(unsigned int area0, unsigned int area1, unsigned int area2) {
+	if (area0 <= area1 && area0 <= area2) {
+		return area0;
+	}
+	if (area1 <= area0 && area1 <= area2) {
+		return area1;
+	}
+	return area2;
 }
 
-unsigned int smallest_perimeter (unsigned int l, unsigned int w, unsigned int h) {
-    unsigned int array[2];
-    array[0] = l;
-    if (w<array[0]) {
-        array[1] = array[0];
-        array[0] = w;
-    } else {
-        array[1] = w;
-    }
-    if (h<array[0]) {
-        array[1] = array[0];
-        array[0] = h;
-    } else {
-        if (h<array[1]) {
-            array[1] = h;
-        }
-    }
-    return 2*array[0] + 2*array[1];
+unsigned int smallest_perimeter(unsigned int l, unsigned int w, unsigned int h) {
+	unsigned int array[2];
+	array[0] = l;
+	
+	if (w < array[0]) {
+		array[1] = l;
+		array[0] = w;
+	} else {
+		array[1] = w;
+	}
+	
+	if (h < array[0]) {
+		array[1] = array[0];
+		array[0] = h;
+	} else if (h < array[1]) {
+		array[1] = h;
+	}
+	
+	return 2 * array[0] + 2 * array[1];
 }
 
 int main() {
-  char buffer[128];
-  unsigned int total_wrap = 0;
-  unsigned int total_ribbon = 0;
-  unsigned int l = 0;
-  unsigned int w = 0;
-  unsigned int h = 0;
+	char buffer[128];
+	unsigned int total_wrap = 0;
+	unsigned int total_ribbon = 0;
+	unsigned int l = 0;
+	unsigned int w = 0;
+	unsigned int h = 0;
 
-  FILE *inputfile = fopen("day2.txt", "r");
-  if (inputfile == NULL) {
-    perror("Error: failed to open file");
-    return 1;
-  }
+	FILE *inputfile = fopen("day2.txt", "r");
+	if (inputfile == NULL) {
+		perror("Error: failed to open file");
+		return 1;
+	}
 
-  while (fgets(buffer, sizeof(buffer), inputfile) != NULL) {
-    if (buffer[strlen(buffer) - 1] == '\n') {
-      buffer[strlen(buffer) - 1] = '\0';
-    }
+	while (fgets(buffer, sizeof(buffer), inputfile) != NULL) {
+		if (buffer[strlen(buffer) - 1] == '\n') {
+			buffer[strlen(buffer) - 1] = '\0';
+		}
 
-    sscanf(buffer, "%ux%ux%u", &l, &w, &h);
+		sscanf(buffer, "%ux%ux%u", &l, &w, &h);
 
-    total_wrap += 2*l*w + 2*w*h + 2*h*l + smallest_area(l*w, w*h, h*l);
-    total_ribbon += smallest_perimeter(l, w, h) + l*w*h; /* Shortest perimeter plus volume */
-  }
+		total_wrap += 2*l*w + 2*w*h + 2*h*l + smallest_area(l*w, w*h, h*l);
+		total_ribbon += smallest_perimeter(l, w, h) + l*w*h; /* Shortest perimeter plus volume */
+	}
 
-  printf("Total wrapping paper: %u\n", total_wrap);
-  printf("Total ribbon: %u\n", total_ribbon);
+	printf("Total wrapping paper: %u\n", total_wrap);
+	printf("Total ribbon: %u\n", total_ribbon);
 }
